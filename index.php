0e3bbc2682f75f063d48286f6ce063774e2ed190
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-03 15:51:29 +0200

    added data

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
-<?php ; ?>
+<?php
 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
 <!DOCTYPE html>
 <html lang="it">
   <head>
@@ -17,7 +58,7 @@
   <body>
     <div class="container-md">
       <h1 class="py-4">HOTELS WEBPAGE</h1>
-
+      
     </div>
   </body>
 </html>
